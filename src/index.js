36368e6e3534509f91c0277b2309c2492cabbdce
Blake Eriks <eblake98@vt.edu>
2021-12-25 17:55:13 -0800

    Dependency nesting

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter } from "react-router-dom"
+import { RecoilRoot } from 'recoil';
+import './index.css'
 import App from './App';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      keepPreviousData: true,
+      staleTime: Infinity,
+      refetchOnWindowFocus: false,
+    }
+  },
+})
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <RecoilRoot>
+      <BrowserRouter>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </BrowserRouter>
+    </RecoilRoot>  
+  </QueryClientProvider>,
   document.getElementById('root')
 );
