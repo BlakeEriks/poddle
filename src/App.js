5f39e4aff3d13d3d436a2baa7406e98065d44fb3
Blake Eriks <eblake98@vt.edu>
2021-12-26 20:56:42 -0800

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import './App.css';
-import { Routes,  Route } from "react-router-dom";
+import { Routes,  Route, Navigate } from "react-router-dom";
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Explore from './pages/Explore';
 import MyList from './pages/MyList';
 import Home from './pages/Home';
+import { useAuthState } from './hooks/auth';
+import Login from './pages/Login';
 
 function App() {
+
+  const [auth, setAuth] = useAuthState()
+
   return (
     <>
     <Header />
     <Routes>
       <Route index element={<Home />} />
+      <Route path="login" element={<Login />} />
+      <Route path="sign-up" element={<Login newUser={true}/>} />
       <Route path="explore" element={<Explore />} />
-      <Route path="my-list" element={<MyList />} />
+      <Route path="my-list" element={auth ? <MyList /> : <Navigate to="/login"/>} />
     </Routes>
     <Footer />
     </>
