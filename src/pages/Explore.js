ae9e9cf2757e9fc427f5bc3ebd6a50dc84220519
Blake Eriks <eblake98@vt.edu>
2021-12-26 20:57:38 -0800

    Changes based on hook changes

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,15 +1,33 @@
+import { useState, useEffect } from "react"
 import Podcasts from "../components/Podcasts"
-import { useTopPodcasts } from "../hooks/podcast"
+import usePodcasts from "../hooks/podcast"
 
 const Explore = () => {
 
-    const { podcasts } = useTopPodcasts()
+    const { getTopPodcasts, getSearchPodcasts } = usePodcasts()
+    const [podcasts, setPodcasts] = useState([])
 
-    console.log(podcasts)
+    const [searchString, setSearchString] = useState('')
+
+    useEffect( async () => {
+        setPodcasts(await getTopPodcasts())
+    }, [])
+
+    const onSubmit = async event => {
+        event.preventDefault()
+        if (!searchString.length) return
+        setPodcasts(await getSearchPodcasts(searchString))
+    }
 
     return (
         <div className="w-full flex flex-wrap flex-col items-center">
-            <input placeholder="Search"/>
+            <form onSubmit={onSubmit}>
+                <input
+                    placeholder="Search" 
+                    value={searchString}
+                    onChange={event => setSearchString(event.target.value)}
+                />
+            </form>
             <Podcasts podcasts={podcasts}/>
         </div>
     )
