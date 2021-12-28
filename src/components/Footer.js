fb707a89bc5e8d108c2598c173cd24a7bad8e011
Blake Eriks <eblake98@vt.edu>
2021-12-28 11:37:01 -0800

    Craete footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,12 @@
+import listenNotes from '../img/powered-by-listen-notes.png'
+
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className="py-4 text-center flex flex-col items-center bg-pink-300">
+            <div className="text-xl font-semibold">
+                Created by Blake Eriks
+            </div>
+            <img src={listenNotes} className="w-40"/>
         </div>
     )
 }
