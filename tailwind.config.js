ea69997b4b6db9f977514a0e37ea44858f4fd51c
Blake Eriks <eblake98@vt.edu>
2022-01-02 19:57:29 -0800

    Change theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,23 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
       'head': ['Kite One'],
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      pink: colors.pink,
+      'green': '#A6DBA1',
     }
   },
   variants: {
