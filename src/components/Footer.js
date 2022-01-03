1c4d7cf29ec119339499f367d76b2cdc2061be8b
Blake Eriks <eblake98@vt.edu>
2022-01-02 20:03:04 -0800

    Restyle footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,9 @@ import listenNotes from '../img/powered-by-listen-notes.png'
 
 const Footer = () => {
     return (
-        <div className="py-4 text-center flex flex-col items-center bg-pink-300">
-            <div className="text-xl font-semibold">
-                Created by Blake Eriks
+        <div className="py-4 text-center flex flex-col items-center">
+            <div className="text-lg">
+                Created by <span className="font-semibold">Blake Eriks</span>
             </div>
             <img src={listenNotes} className="w-40"/>
         </div>
