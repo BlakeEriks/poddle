779e6bea00efb75d3e82467960e469a0b6a78b8b
Blake Eriks <eblake98@vt.edu>
2022-01-02 20:00:23 -0800

    Change explore route to top

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
-import { Routes,  Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Footer from './components/Footer';
 import Header from './components/Header';
-import Explore from './pages/Explore';
 import MyList from './pages/MyList';
 import Home from './pages/Home';
 import { useAuthState } from './hooks/auth';
 import Login from './pages/Login';
 import Podcast from './pages/Podcast';
+import Top from './pages/Top';
 
 function App() {
 
@@ -23,7 +23,7 @@ function App() {
         <Route path="login" element={<Login />} />
         <Route path="sign-up" element={<Login newUser={true}/>} />
         <Route path="podcasts/:id" element={<Podcast />} />
-        <Route path="explore" element={<Explore />} />
+        <Route path="top" element={<Top />} />
         <Route path="my-list" element={auth ? <MyList /> : <Navigate to="/login"/>} />
       </Routes>
       <Footer />
