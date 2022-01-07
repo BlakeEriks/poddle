98f3783e596c9449fb1636f0e838ac7d7948acfa
Blake Eriks <eblake98@vt.edu>
2022-01-06 16:37:20 -0800

    Add fade + move effect on page switch. Scroll top

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import Footer from './components/Footer';
 import Header from './components/Header';
 import MyList from './pages/MyList';
@@ -11,25 +11,42 @@ import Podcast from './pages/Podcast';
 import Top from './pages/Top';
 import Genres from './pages/Genres';
 import Recommended from './pages/Recommended';
+import { CSSTransition } from "react-transition-group";
+import ClipLoader from "react-spinners/ClipLoader";
+import { useLoadingState } from './hooks/loading';
+import { useEffect } from 'react';
 
 function App() {
 
   const [auth] = useAuthState()
+  const location = useLocation()
+  const { loading } = useLoadingState()
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
 
   return (
     <>
       <Header />
-        <Routes>
-          <Route index element={<Navigate to="home" />} />
-          <Route path="home" element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="sign-up" element={<Login newUser={true}/>} />
-          <Route path="top" element={<Top />} />
-          <Route path="recommended" element={<Recommended />} />
-          <Route path="my-list" element={auth ? <MyList /> : <Navigate to="/login"/>} />
-          <Route path="my-genres" element={auth ? <Genres /> : <Navigate to="/login"/>} />
-          <Route path="podcasts/:id" element={<Podcast />} />
-        </Routes>
+      <CSSTransition key={location.key} in={false} timeout={0}>
+        <div className={"transition-all duration-700 relative " + (loading ? "opacity-0 top-5" : "opacity-100 top-0")}>
+          <Routes>
+              <Route index element={<Navigate to="/home" />} />
+              <Route path="home" element={<Home />} />
+              <Route path="login" element={<Login />} />
+              <Route path="sign-up" element={<Login newUser={true}/>} />
+              <Route path="top" element={<Top />} />
+              <Route path="recommended" element={<Recommended />} />
+              <Route path="my-list" element={auth ? <MyList /> : <Navigate to="/login"/>} />
+              <Route path="my-genres" element={auth ? <Genres /> : <Navigate to="/login"/>} />
+              <Route path="podcasts/:id" element={<Podcast />} />
+          </Routes>
+        </div>
+      </CSSTransition>
+      <div className={"flex absolute justify-center border-gray-300 w-full transition-all duration-200 " + (loading ? "opacity-100" : "opacity-0")} style={{top: '30vh'}}>
+        <ClipLoader color="#A8A8A8" size={150} />
+      </div>
       <Footer />
     </>
   );
