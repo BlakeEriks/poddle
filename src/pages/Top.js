3f9392bbcd2faa6a31e54edc95a9f58b1182a6b4
Blake Eriks <eblake98@vt.edu>
2022-01-06 16:50:57 -0800

    Add image loading callback

--- src/pages/Top.js
+++ src/pages/Top.js
@@ -1,11 +1,13 @@
-import { useState, useEffect } from "react"
 import Podcasts from "../components/Podcasts"
-import {usePodcasts} from "../hooks/podcast"
+import usePodcasts from "../hooks/podcast"
 import guyImg from '../img/guy1.png'
+import OnImagesLoaded from 'react-on-images-loaded';
+import { useLoadingState } from '../hooks/loading';
 
 const Top = () => {
 
     const { topPodcasts } = usePodcasts()
+    const {loading, setLoading} = useLoadingState()
     // const [searchString, setSearchString] = useState('')
 
     // const onSubmit = async event => {
@@ -15,7 +17,9 @@ const Top = () => {
     // }
 
     return (
-        <>
+        <OnImagesLoaded 
+            onLoaded={() => setLoading(false)}
+        >
             <div className="flex flex-row items-center justify-center p-24 overflow-hidden border-b border-gray-300">
                 <div className="w-96 p-10">
                     <img src={guyImg} alt="guy listening" />
@@ -38,7 +42,7 @@ const Top = () => {
                 </form> */}
                 <Podcasts podcasts={topPodcasts} />
             </div>
-        </>
+        </OnImagesLoaded>
     )
 }
 
