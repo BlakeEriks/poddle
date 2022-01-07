6cc9e5a4c6a45280c083ae3db8e0c364e565cf34
Blake Eriks <eblake98@vt.edu>
2022-01-06 16:38:20 -0800

    reformat

--- src/hooks/genre.js
+++ src/hooks/genre.js
@@ -15,7 +15,7 @@ const useGenres = () => {
         'genres', 
         async () => await http.get(`${API_BASE_URL}/genres`)
     )
-
+    
     const myGenresQuery = useQuery('genres/my-list', async () => await http.get(`${API_BASE_URL}/genres/my_list`), {enabled: !!auth} )
 
     const genreMutation = useMutation( ids => {
@@ -31,7 +31,7 @@ const useGenres = () => {
             allGenres: allGenresQuery?.data,
             myGenres: myGenresQuery?.data, 
             updateGenres: genreMutation.mutate
-        }
+    }
 
 }
 
